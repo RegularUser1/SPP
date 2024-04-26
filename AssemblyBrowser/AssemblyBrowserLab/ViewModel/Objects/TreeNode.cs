e741cedb9e1dfaa9dc3107c519bd75eacd054ec8
Kozyr <oralcumshot677@proton.me>
2024-04-26 20:51:10 +0300

    UI done

---
+++ AssemblyBrowser/AssemblyBrowserLab/ViewModel/Objects/TreeNode.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AssemblyBrowserLab.ViewModel.Objects
+{
+    public class TreeNode(string text)
+    {
+        public string Text { get; } = text;
+        public ObservableCollection<TreeNode> Children { get; } = [];
+    }
+}
